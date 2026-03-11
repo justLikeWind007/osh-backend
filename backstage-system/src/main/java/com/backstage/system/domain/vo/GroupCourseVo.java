12e874eed1ae5d2b6d69b96e68ea70d7240d5fae
jzcl-one <3891715998@qq.com>
2026-03-11 21:26:58 +0800

    group

---
+++ backstage-system/src/main/java/com/backstage/system/domain/vo/GroupCourseVo.java
@@ -0,0 +1,32 @@
+package com.backstage.system.domain.vo;
+
+import com.backstage.system.domain.course.SysCourse;
+import com.backstage.system.domain.group.GroupActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 九转苍翎
+ * Date: 2026/3/3
+ * Time: 20:38
+ */
+public class GroupCourseVo extends SysCourse {
+
+    /** 拼团活动信息 */
+    private GroupActivity group;
+
+    public GroupActivity getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupActivity groupActivity) {
+        this.group = groupActivity;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupCourseVo{" +
+                ", group=" + group +
+                '}';
+    }
+}
