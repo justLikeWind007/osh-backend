26c699d97625d70d5d77259e50ec2d858de46a38
czishere <13999553+czishere@user.noreply.gitee.com>
2026-03-12 23:04:02 +0800

    未完成剩余：订单模块：下单前获取产品（差个电子书）

--- backstage-system/src/main/java/com/backstage/system/controller/order/OshUploadImageController.java
+++ backstage-system/src/main/java/com/backstage/system/controller/order/OshUploadImageController.java
@@ -2,6 +2,8 @@ package com.backstage.system.controller.order;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+
+import com.backstage.common.annotation.Anonymous;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,10 @@ import com.backstage.system.domain.order.OshUploadImage;
 import com.backstage.system.service.order.IOshUploadImageService;
 import com.backstage.common.utils.poi.ExcelUtil;
 import com.backstage.common.core.page.TableDataInfo;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+import com.backstage.common.exception.file.FileUploadException;
+import com.backstage.common.utils.file.OssUtil;
 
 /**
  * 图片上传记录Controller
@@ -33,6 +39,9 @@ public class OshUploadImageController extends BaseController
 {
     @Autowired
     private IOshUploadImageService oshUploadImageService;
+
+    @Autowired
+    private OssUtil ossUtil;
 
     /**
      * 查询图片上传记录列表
@@ -70,14 +79,71 @@ public class OshUploadImageController extends BaseController
     }
 
     /**
+     * 上传图片到阿里云 OSS
+     */
+    @Log(title = "上传图片", businessType = BusinessType.INSERT)
+    @Anonymous
+    @PostMapping("/image")
+    public AjaxResult uploadImage(MultipartFile file) throws Exception
+    {
+        if (file == null || file.isEmpty()) {
+            return error("上传文件不能为空");
+        }
+
+        String url = ossUtil.uploadFile(file);
+
+        OshUploadImage uploadImage = new OshUploadImage();
+        uploadImage.setUserId(1L);
+        uploadImage.setSchoolId(1L);
+        uploadImage.setFileName(file.getOriginalFilename());
+        uploadImage.setFilePath(url);
+        uploadImage.setFileSize(file.getSize());
+        uploadImage.setFileType(file.getContentType());
+        uploadImage.setStatus(1L);
+
+        int result = oshUploadImageService.insertOshUploadImage(uploadImage);
+        
+        if (result > 0) {
+            AjaxResult ajax = AjaxResult.success();
+            ajax.put("url", url);
+            ajax.put("fileName", file.getOriginalFilename());
+            ajax.put("fileSize", file.getSize());
+            return ajax;
+        } else {
+            return error("上传失败");
+        }
+    }
+
+    /**
+     * 删除图片（同时删除 OSS 上的文件）
+     */
+    @PreAuthorize("@ss.hasPermi('system:image:remove')")
+    @Log(title = "图片上传记录", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids)
+    {
+        for (Long id : ids) {
+            OshUploadImage uploadImage = oshUploadImageService.selectOshUploadImageById(id);
+            if (uploadImage != null && uploadImage.getFilePath() != null) {
+                ossUtil.deleteFile(uploadImage.getFilePath());
+            }
+        }
+        
+        int result = oshUploadImageService.deleteOshUploadImageByIds(ids);
+        return toAjax(result);
+    }
+
+    /**
      * 新增图片上传记录
      */
-    @PreAuthorize("@ss.hasPermi('system:image:add')")
+//    @PreAuthorize("@ss.hasPermi('system:image:add')")
+    @Anonymous
     @Log(title = "图片上传记录", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody OshUploadImage oshUploadImage)
+    public AjaxResult add(@RequestBody MultipartRequest file)
     {
         //TODO 上传图片
+        OshUploadImage oshUploadImage = new OshUploadImage();
         return toAjax(oshUploadImageService.insertOshUploadImage(oshUploadImage));
     }
 
@@ -90,16 +156,5 @@ public class OshUploadImageController extends BaseController
     public AjaxResult edit(@RequestBody OshUploadImage oshUploadImage)
     {
         return toAjax(oshUploadImageService.updateOshUploadImage(oshUploadImage));
-    }
-
-    /**
-     * 删除图片上传记录
-     */
-    @PreAuthorize("@ss.hasPermi('system:image:remove')")
-    @Log(title = "图片上传记录", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{ids}")
-    public AjaxResult remove(@PathVariable Long[] ids)
-    {
-        return toAjax(oshUploadImageService.deleteOshUploadImageByIds(ids));
     }
 }
