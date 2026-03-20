7700704d38d3adb9f93faf5e38ad66ad863a485a
czishere <13999553+czishere@user.noreply.gitee.com>
2026-03-20 18:15:48 +0800

    删除阿里云的OSS

--- backstage-common/src/main/java/com/backstage/common/config/properties/CloudflareR2Config.java
+++
@@ -1,8 +0,0 @@
-package com.backstage.common.config.properties;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CloudflareR2Config {
-    // 什么都不用写！
-}
