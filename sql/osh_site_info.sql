01f3b37887c215da94b7008bda7073b87ef1cf2a
hook <276874643@qq.com>
2026-04-15 00:19:19 +0800

    1

--- sql/osh_site_info.sql
+++ sql/osh_site_info.sql
@@ -4,17 +4,19 @@
 DROP TABLE IF EXISTS `osh_site_info`;
 CREATE TABLE `osh_site_info`
 (
-    `id`          bigint unsigned                         NOT NULL AUTO_INCREMENT COMMENT '网站编号（主键）',
-    `site_name`   varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '网站名称',
-    `cover`       varchar(500) COLLATE utf8mb4_unicode_ci          DEFAULT NULL COMMENT '网站封面地址',
-    `site_url`    varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '网站访问路径',
-    `description` varchar(2000) COLLATE utf8mb4_unicode_ci         DEFAULT NULL COMMENT '网站描述信息',
-    `status`      tinyint                                          DEFAULT '1' COMMENT '状态：1=启用，0=禁用',
-    `created_by`  bigint unsigned                         NOT NULL COMMENT '创建人ID/账号',
-    `create_time` timestamp                               NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_by`   bigint unsigned                                  DEFAULT NULL COMMENT '更新人ID/账号',
-    `update_time` timestamp                               NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `is_deleted`  tinyint                                 NOT NULL DEFAULT '0' COMMENT '是否删除：0=未删除，1=已删除',
+    `id`                bigint unsigned                         NOT NULL AUTO_INCREMENT COMMENT '网站编号（主键）',
+    `site_name`         varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '网站名称',
+    `cover`             varchar(500) COLLATE utf8mb4_unicode_ci          DEFAULT NULL COMMENT '网站封面地址',
+    `site_url`          varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '网站访问路径',
+    `description`       varchar(2000) COLLATE utf8mb4_unicode_ci         DEFAULT NULL COMMENT '网站描述信息',
+    `status`            tinyint                                          DEFAULT '1' COMMENT '状态：1=启用，0=禁用',
+    `created_by`        bigint unsigned                         NOT NULL COMMENT '创建人ID/账号',
+    `create_time`       timestamp                               NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `last_check_time`   timestamp                               NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '上次检查时间',
+    `last_check_status` varchar(100) COMMENT '上次检查状态',
+    `update_by`         bigint unsigned                                  DEFAULT NULL COMMENT '更新人ID/账号',
+    `update_time`       timestamp                               NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `is_deleted`        tinyint                                 NOT NULL DEFAULT '0' COMMENT '是否删除：0=未删除，1=已删除',
     PRIMARY KEY (`id`),
     KEY `idx_site_name` (`site_name`, `is_deleted`)
 ) ENGINE = InnoDB
