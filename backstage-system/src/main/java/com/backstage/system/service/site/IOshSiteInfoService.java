d0391d88264a181d71dd2b28ea5864eda1f48dc5
hook <276874643@qq.com>
2026-04-20 23:10:33 +0800

    feat: 网站异常状态邮件通知

--- backstage-system/src/main/java/com/backstage/system/service/site/IOshSiteInfoService.java
+++ backstage-system/src/main/java/com/backstage/system/service/site/IOshSiteInfoService.java
@@ -1,10 +1,14 @@
 package com.backstage.system.service.site;
 
 import com.backstage.system.domain.site.OshSiteInfo;
+import com.backstage.system.domain.site.OshSiteMaintainer;
 import com.backstage.system.domain.user.OshUser;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.util.MultiValueMap;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 内部网站信息 Service 接口
@@ -15,11 +19,11 @@ public interface IOshSiteInfoService extends IService<OshSiteInfo> {
 
     /**
      * 新增网站使用记录
-     * 
+     *
      * @param siteInfo 网站信息
      * @return 结果
      */
-  int insertUsage(OshSiteInfo siteInfo, OshUser oshUser);
+    int insertUsage(OshSiteInfo siteInfo, OshUser oshUser);
 
     boolean saveSiteInfo(OshSiteInfo siteInfo);
 
@@ -27,7 +31,11 @@ public interface IOshSiteInfoService extends IService<OshSiteInfo> {
 
     List<OshSiteInfo> listSites(OshSiteInfo siteInfo);
 
+    MultiValueMap<Long, OshSiteMaintainer> getSiteMaintainers(Collection<Long> siteIds);
+
     boolean testConnection(List<OshSiteInfo> oshSiteInfos);
 
     boolean testConnection(List<OshSiteInfo> oshSiteInfos, int timeout);
+
+    List<OshSiteInfo> checkConnectionStatus(List<OshSiteInfo> oshSiteInfos, int timeout);
 }
--- backstage-system/src/main/java/com/backstage/system/service/site/impl/OshSiteInfoServiceImpl.java
+++ backstage-system/src/main/java/com/backstage/system/service/site/impl/OshSiteInfoServiceImpl.java
@@ -7,6 +7,7 @@ import com.backstage.system.domain.site.OshSiteUsage;
 import com.backstage.system.domain.user.OshUser;
 import com.backstage.system.mapper.site.OshSiteInfoMapper;
 import com.backstage.system.mapper.site.OshSiteMaintainerMapper;
+import com.backstage.system.mapper.user.OshUserMapper;
 import com.backstage.system.service.site.IOshSiteInfoService;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
@@ -16,13 +17,23 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -40,6 +51,21 @@ public class OshSiteInfoServiceImpl extends ServiceImpl<OshSiteInfoMapper, OshSi
 
     @Autowired
     private OshSiteMaintainerMapper oshSiteMaintainerMapper;
+
+    @Autowired
+    private OshUserMapper userMapper;
+
+    @Value(value = "${backstage.site.test.maxConnectionTimeout:5000}")
+    private int maxConnectionTimeout;
+
+    @Value("${email.from}")
+    private String from;
+
+    @Autowired
+    ThreadPoolTaskExecutor taskExecutor;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     /**
      * 新增网站使用记录
@@ -91,16 +117,7 @@ public class OshSiteInfoServiceImpl extends ServiceImpl<OshSiteInfoMapper, OshSi
 
     @Override
     public List<OshSiteInfo> listSites(OshSiteInfo siteInfo) {
-        List<OshSiteInfo> list = this.lambdaQuery()
-                .select(OshSiteInfo::getId,
-                        OshSiteInfo::getSiteName,
-                        OshSiteInfo::getCover,
-                        OshSiteInfo::getDescription,
-                        OshSiteInfo::getStatus,
-                        OshSiteInfo::getLastCheckTime)
-                .like(StringUtils.isNoneBlank(siteInfo.getSiteName()), OshSiteInfo::getSiteName, siteInfo.getSiteName())
-                .eq(siteInfo.getStatus() != null, OshSiteInfo::getStatus, siteInfo.getStatus())
-                .list();
+        List<OshSiteInfo> list = this.lambdaQuery().select(OshSiteInfo::getId, OshSiteInfo::getSiteName, OshSiteInfo::getCover, OshSiteInfo::getDescription, OshSiteInfo::getStatus, OshSiteInfo::getLastCheckTime).like(StringUtils.isNoneBlank(siteInfo.getSiteName()), OshSiteInfo::getSiteName, siteInfo.getSiteName()).eq(siteInfo.getStatus() != null, OshSiteInfo::getStatus, siteInfo.getStatus()).list();
 
         setMaintainers(list);
 
@@ -112,16 +129,11 @@ public class OshSiteInfoServiceImpl extends ServiceImpl<OshSiteInfoMapper, OshSi
             return;
         }
         Set<Long> siteIds = list.stream().map(OshSiteInfo::getId).collect(Collectors.toSet());
-        Map<Long, List<OshSiteMaintainer>> maintainersMap = oshSiteInfoMapper.selectMaintainersBySiteIds(siteIds)
-                .stream()
-                .collect(Collectors.groupingBy(OshSiteMaintainer::getSiteId));
+        Map<Long, List<OshSiteMaintainer>> maintainersMap = oshSiteInfoMapper.selectMaintainersBySiteIds(siteIds).stream().collect(Collectors.groupingBy(OshSiteMaintainer::getSiteId));
 
         for (OshSiteInfo oshSiteInfo : list) {
             List<OshSiteMaintainer> maintainers = maintainersMap.getOrDefault(oshSiteInfo.getId(), Collections.emptyList());
-            oshSiteInfo.setMaintainerUserIds(maintainers.stream()
-                    .map(OshSiteMaintainer::getUserId)
-                    .map(String::valueOf)
-                    .collect(Collectors.toList()));
+            oshSiteInfo.setMaintainerUserIds(maintainers.stream().map(OshSiteMaintainer::getUserId).map(String::valueOf).collect(Collectors.toList()));
             oshSiteInfo.setMaintainers(maintainers);
         }
     }
@@ -130,19 +142,98 @@ public class OshSiteInfoServiceImpl extends ServiceImpl<OshSiteInfoMapper, OshSi
      * 每10分钟测试1次
      */
     @Scheduled(cron = "0 0/10 * * * ?")
+    @Transactional(rollbackFor = Exception.class)
     public void testSiteInfoResponseStatus() {
-        List<OshSiteInfo> oshSiteInfos = oshSiteInfoMapper.selectList(Wrappers.<OshSiteInfo>lambdaQuery()
-                .eq(OshSiteInfo::getDeleteFlag, 0));
-        testConnection(oshSiteInfos);
+        try {
+            List<OshSiteInfo> oshSiteInfos = oshSiteInfoMapper.selectList(Wrappers.<OshSiteInfo>lambdaQuery().eq(OshSiteInfo::getDeleteFlag, 0));
+            List<OshSiteInfo> siteList = checkConnectionStatus(oshSiteInfos, maxConnectionTimeout);
+            updateBatchById(siteList);
+            List<OshSiteInfo> failedList = siteList.stream().filter(site -> site.getLastCheckStatus() == 0).collect(Collectors.toList());
+            if (CollectionUtils.isNotEmpty(failedList)) {
+                sendEmail(failedList);
+            }
+        } catch (Throwable throwable) {
+            log.error("测试网站状态异常", throwable);
+        }
+    }
+
+    private void sendEmail(List<OshSiteInfo> failedList) {
+        List<Long> siteIds = failedList.stream().map(OshSiteInfo::getId).collect(Collectors.toList());
+        MultiValueMap<Long, OshSiteMaintainer> siteMaintainers = getSiteMaintainers(siteIds);
+        MultiValueMap<Long, OshSiteInfo> userIdSiteMap = new LinkedMultiValueMap<>();
+        for (OshSiteInfo oshSiteInfo : failedList) {
+            List<OshSiteMaintainer> maintainers = siteMaintainers.get(oshSiteInfo.getId());
+            if (CollectionUtils.isEmpty(maintainers)) {
+                continue;
+            }
+            for (OshSiteMaintainer maintainer : maintainers) {
+                Long userId = maintainer.getUserId();
+                userIdSiteMap.add(userId, oshSiteInfo);
+            }
+        }
+        final Map<Long, OshUser> userMap = userMapper.selectList(Wrappers.<OshUser>lambdaQuery()
+                        .in(OshUser::getId, userIdSiteMap.keySet()))
+                .stream()
+                .collect(Collectors.toMap(OshUser::getId, Function.identity()));
+        if (CollectionUtils.isEmpty(userMap)) {
+            return;
+        }
+        for (Map.Entry<Long, List<OshSiteInfo>> entry : userIdSiteMap.entrySet()) {
+            Long userId = entry.getKey();
+            OshUser user = userMap.get(userId);
+            StringBuilder emailContent = new StringBuilder("用户" + user.getUsername() + "，您负责的如下网站有异常，请及时处理：\n");
+            int i = 1;
+            for (OshSiteInfo site : entry.getValue()) {
+                emailContent.append(i++).append("：");
+                emailContent.append(site.getSiteName()).append("：").append(site.getSiteUrl()).append("\n");
+            }
+            CompletableFuture.runAsync(() -> {
+                try {
+                    sendEmail("网站异常通知", from, emailContent.toString(), user.getEmail());
+                } catch (MessagingException e) {
+                    LOG.info("发送网站异常通知邮件成功: {}", user.getEmail());
+                }
+            }, taskExecutor).whenComplete((v, t) -> LOG.info("发送网站异常通知邮件成功 {}", user.getEmail()));
+        }
+    }
+
+    private void sendEmail(String subject, String from, String content, String receiver) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        // 设置邮件内容
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(content, true);
+        mimeMessageHelper.setTo(receiver);
+        mimeMessageHelper.setFrom(from);
+        javaMailSender.send(mimeMessage);
     }
 
     @Override
+    public MultiValueMap<Long, OshSiteMaintainer> getSiteMaintainers(Collection<Long> siteIds) {
+        List<OshSiteMaintainer> maintainers = oshSiteMaintainerMapper.selectList(siteIds);
+        MultiValueMap<Long, OshSiteMaintainer> siteMaintainers = new LinkedMultiValueMap<>();
+        for (OshSiteMaintainer maintainer : maintainers) {
+            siteMaintainers.add(maintainer.getSiteId(), maintainer);
+        }
+        return siteMaintainers;
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
     public boolean testConnection(List<OshSiteInfo> oshSiteInfos) {
-        return testConnection(oshSiteInfos, 5000);
+        return testConnection(oshSiteInfos, maxConnectionTimeout);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public boolean testConnection(List<OshSiteInfo> oshSiteInfos, int timeout) {
+        List<OshSiteInfo> siteList = checkConnectionStatus(oshSiteInfos, timeout);
+        updateBatchById(siteList);
+        return true;
+    }
+
+    @Override
+    public List<OshSiteInfo> checkConnectionStatus(List<OshSiteInfo> oshSiteInfos, int timeout) {
         for (OshSiteInfo siteInfo : oshSiteInfos) {
             if (StringUtils.isBlank(siteInfo.getSiteUrl())) {
                 continue;
@@ -160,12 +251,11 @@ public class OshSiteInfoServiceImpl extends ServiceImpl<OshSiteInfoMapper, OshSi
                 }
                 siteInfo.setLastCheckStatus(lastCheckStatus);
                 siteInfo.setLastCheckTime(new Date());
-                oshSiteInfoMapper.updateById(siteInfo);
             } catch (Throwable throwable) {
                 LOG.error("failed to test site connection, {}", JSON.toJSONString(siteInfo));
             }
         }
-        return true;
+        return oshSiteInfos;
     }
 
     /**
