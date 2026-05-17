e1a49e4cfe8274841ea3e1d9598e8e1dac29388e
Whiskey_Lau <1373463511@qq.com>
2026-05-17 18:09:11 +0800

    feat(common): 添加支付配置类
    - 新增PayConfig类用于管理支付相关配置
    - 配置商户ID、商户KEY等基础支付参数
    - 设置易支付接口URL和订单查询接口
    - 添加支付超时时间和回调地址配置
    - 配置支付成功跳转地址常量

--- backstage-common/src/main/java/com/backstage/common/config/PayConfig.java
+++ backstage-common/src/main/java/com/backstage/common/config/PayConfig.java
@@ -39,13 +39,13 @@ public class PayConfig {
     /**
      * 支付超时时间（分钟）
      */
-    @Value("${pay.expire-minutes:30}")
+    @Value("${pay.expire-minutes:5}")
     public Integer EXPIRE_MINUTES;
 
     /**
      * 回调地址 需要回调到后端的话，内网无法进来
      */
-    @Value("${pay.notify-url}")
+    @Value("${pay.notify-url:}")
     public String NOTIFY_URL;
 
     // 支付成功的跳转地址 (感觉没必要，前端直接轮询自己跳转就好了)
