f880fd9d81c9ba1bb7a5bcae4c58439c9fd70951
Tianyi <willorn@qq.com>
2026-05-31 11:56:40 +0800

    refactor: 反馈后端接口路由错误

--- backstage-system/src/main/java/com/backstage/system/controller/assistant/AssistantFeedbackPublicController.java
+++ backstage-system/src/main/java/com/backstage/system/controller/assistant/AssistantFeedbackPublicController.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Anonymous
 @Api(tags = "AI助手反馈-公开接口")
 @RestController
-@RequestMapping("/pc/public/feedback")
+@RequestMapping("/pc/feedback")
 public class AssistantFeedbackPublicController extends BaseController {
 
     public AssistantFeedbackPublicController(IAssistantFeedbackCategoryService categoryService, IAssistantFeedbackService feedbackService, IAssistantFeedbackCommentService commentService, IAssistantFeedbackTagService feedbackTagService, IAnnouncementMarqueeQueryService announcementMarqueeQueryService) {
